ee680526e2afb9a45ca4d252b776c01761f29142
Joerg Weingrill <jweingrill@gmail.com>
2013-04-30 17:51:34 +0200

    minor changes in bsc

--- src/bsc.py
+++ src/bsc.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     tab = hdulist[1].data
     hdulist.close()
     #tab = tab[::5]
-    i = where(tab['Vmag']<6.5)
+    i = where(tab['Vmag']<7.0)
     tab = tab[i]
     ra = tab['RAJ2000']
     dec = tab['DEJ2000']
@@ -27,13 +27,13 @@ if __name__ == '__main__':
     
     bv = clip(bv,-0.3,1.52) # B0 to M4
     
-    pts = scaleto(vmag,[40.0,1.0])
+    pts = scaleto(vmag,[80.0,1.0])
     print min(vmag),max(vmag),len(vmag)
     #plt.set_cmap('Spectral')
     plt.subplot(111, axisbg='darkblue')
     #Plejades, Hyades 03 47 00 +24 07.0
     plt.xlim(75.,50.)
     plt.ylim(10.,30.)
-    plt.grid()
+    plt.grid(color='w')
     plt.scatter(ra, dec, s=pts, c=-bv, edgecolor='none', cmap=pylab.cm.Spectral)
     plt.show()
