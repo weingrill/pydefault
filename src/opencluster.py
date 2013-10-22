793ecd0e5597c22ec1793315e139ca46fcb2ea40
Joerg Weingrill <jweingrill@aip.de>
2013-10-22 17:50:44 +0200

    added tycho to retrieve Tycho stars from the database; opencluster: added obsmode

--- src/opencluster.py
+++ src/opencluster.py
@@ -8,7 +8,12 @@ class OpenCluster(object):
     classdocs
     '''
 
-    def __init__(self, objectname='', uname=''):
+    def __init__(self, objectname='', uname='', ra=None, dec=None, obsmode=None):
+        """
+        initializes the OpenCluster Object with certain values
+        obsmode can be 'rot' for rotation photometry
+                    or 'cmd' for BVI photometry
+        """
         self.startdate = '2013-03-21'
         self.enddate = '2014-12-31'
         self.telescope = 'STELLA-I'
@@ -34,23 +39,89 @@ class OpenCluster(object):
                        'XBinCCD':1,
                        'YBinCCD':1}
         self.sequence = {'sequence':'FullFilters',
-                        'ExposureTime':200.0,
+                        'ExposureTime':30.0,
                         'ExposureRepeat':15,
                         'ExposureIncrease':'5*1,5*1.5,5*3',
                         'FilterSequence':'5*I,5*V,5*B',
                         'offset':0.0}
         self.object = {'ObjectName':objectname,
-                       'RA':0.0,
-                       'Dec':0.0}
+                       'RA':ra,
+                       'Dec':dec}
         self.constraints = {'MoonDistance.Min':45,
                             'SolHeight.Max':-16.0,
                             'AirmassTarget.Max':2.0,
                             'AltTarget.Min':30.0}
         self.file = ''
-        self.duration = 0
-        self.get_coordiantes()
+        self.filename = ''
+        self._duration = 0
+        self.fields = 1
+
+        if obsmode == 'bvs':
+            self.sequence['ExposureTime'] = 30.0
+            self.sequence['ExposureRepeat'] = 9
+            self.sequence['ExposureIncrease'] = '3*1,3*3'
+            self.sequence['FilterSequence'] = '3*V,3*B'
+            self.mode['pernight'] = 2 # can be refined
+
+        if obsmode == 'bvl':
+            self.sequence['ExposureTime'] = 300.0
+            self.sequence['ExposureRepeat'] = 9
+            self.sequence['ExposureIncrease'] = '3*1,3*3'
+            self.sequence['FilterSequence'] = '3*V,3*B'
+            self.mode['pernight'] = 2 # can be refined
+        
+        if obsmode == 'bv':
+            self.sequence['ExposureTime'] = 30.0
+            self.sequence['ExposureRepeat'] = 12
+            self.sequence['ExposureIncrease'] = '3*1,3*3,3*10,3*10'
+            self.sequence['FilterSequence'] = '3*V,3*B,3*V,3*B'
+            self.mode['pernight'] = 2 # can be refined
+
+        if obsmode == 'bvsl20':
+            self.sequence['ExposureTime'] = 20.0 # 20.0 + 200.0
+            self.sequence['ExposureRepeat'] = 20
+            self.sequence['ExposureIncrease'] = '5*1,5*3,5*10,5*10'
+            self.sequence['FilterSequence'] = '5*V,5*B,5*V,5*B'
+            self.mode['pernight'] = 2 # can be refined
+
+        if obsmode == 'bvsl30':
+            self.sequence['ExposureTime'] = 30.0 # 20.0 + 200.0
+            self.sequence['ExposureRepeat'] = 20
+            self.sequence['ExposureIncrease'] = '5*1,5*3,5*10,5*10'
+            self.sequence['FilterSequence'] = '5*V,5*B,5*V,5*B'
+            self.mode['pernight'] = 2 # can be refined
+
+        
+        if obsmode == 'cmds':
+            self.sequence['ExposureTime'] = 30.0
+            self.sequence['ExposureRepeat'] = 9
+            self.sequence['ExposureIncrease'] = '3*1,3*1.5,3*3'
+            self.sequence['FilterSequence'] = '3*I,3*V,3*B'
+            self.mode['pernight'] = 2 # can be refined
+            
+        if obsmode == 'cmdl':
+            self.sequence['ExposureTime'] = 300.0
+            self.sequence['ExposureRepeat'] = 9
+            self.sequence['ExposureIncrease'] = '3*1,3*1.5,3*3'
+            self.sequence['FilterSequence'] = '3*I,3*V,3*B'
+            self.mode['pernight'] = 2 # can be refined
+
+        if obsmode == 'rot':
+            self.sequence['ExposureTime'] = 30.0
+            self.sequence['ExposureIncrease'] = '1,10'
+            self.sequence['FilterSequence'] = 'V,V'
+            self.mode['timeout'] = 1320000 # duration*fields*1000
+            self.mode['pernight'] = 10 # can be refined
+        
+        
+        
+        if self.object['RA'] is None or self.object['Dec'] is None:
+            self.get_coordiantes()
         
     def get_coordiantes(self):
+        """
+        queries the object coordinates
+        """
         import PySimbad
         import astronomy as ast
         
@@ -67,6 +138,9 @@ class OpenCluster(object):
         self.coords=[self.object['RA'],self.object['Dec']]
         
     def plan_wifsip(self, nfields=4):
+        """
+        returns new subframes
+        """
         d = 1320.2/3600.0
         d2 = d/2
         cra, cdec = self.object['RA'],self.object['Dec']
@@ -84,57 +158,96 @@ class OpenCluster(object):
                  (cra - d2, cdec - d2),
                  (cra + d2, cdec - d2)]
         
+        subframes = []
         for f in fields:
             i = fields.index(f)
-            print "%s_%s: %.5f, %.5f" % (self.object['ObjectName'], names[i],f[0],f[1])
-            
-            
-    def wifsip_fov(self):
-        ra, dec = self.object['RA'],self.object['Dec']
+            subframes.append(OpenCluster(objectname=self.object['ObjectName'],
+                                         uname = '%s %s' % (self.uname, names[i]), 
+                                         ra = f[0], dec = f[1]))
+        for sf in subframes:
+            sf.title = self.title
+            sf.abstract = self.abstract
+            sf.mode = self.mode
+            sf.sequence = self.sequence
         
-        d = 1320.2/3600.0
-        ras = [ra-d/2, ra+d/2, ra+d/2, ra-d/2, ra-d/2]
-        das = [dec-d/2, dec-d/2, dec+d/2, dec+d/2, dec-d/2]
-        plt.plot(ras, das, color)
-    
+        return subframes
+            
+    def plot(self):
+        """
+        plot the fov
+        """
+        import matplotlib.pyplot as plt
+        ra, dec = self.object['RA'],self.object['Dec']
+        d2 = 0.5*1320.2/3600.0
+        ras = [ra-d2, ra+d2, ra+d2, ra-d2, ra-d2]
+        das = [dec-d2, dec-d2, dec+d2, dec+d2, dec-d2]
+        plt.plot(ras, das)
+
+    @property
+    def duration(self):
+        """
+        getter for duration
+        """
+        expt = float(self.sequence['ExposureTime'])
+        seq = self.sequence['ExposureIncrease'] #'5*1,5*1.5,5*3'
+        sseq = seq.split(',')
+        repeat = 0
+        duration = 0.0
+        for s in sseq:
+            if s.find('*')>0:
+                repeat += int(s.split('*')[0])
+                duration += expt*int(s.split('*')[0])*float(s.split('*')[1])
+            else:
+                repeat += 1
+                duration += expt*float(s)
+                
+        self.sequence['ExposureRepeat'] = repeat
+        return duration
+        
     def tofile(self, path = './'):
+        """
+        stores the data to a save file
+        """
         #ngc6940_sw_bvi_20130926.save
         import os
         from datetime import date, datetime
-        self.file = self.uname.lower().replace(' ','_')
+        self.file = self.uname.lower().replace(' ','_')+'.xml'
         filename, ext =  os.path.splitext(self.file)
         todaystr = date.strftime(date.today(),'%Y%m%d')
-        filename = os.path.join(path,filename + '_' + todaystr + '.save')
-        f = open(filename, 'wt')
+        self.filename = os.path.join(path,filename + '_' + todaystr + '.save')
+        def bin(boolean):
+            '''return the binary value to an appropriate string'''
+            return str(boolean).lower()
+        
+        f = open(self.filename, 'wt')
         f.write('#User input\n')
         #Thu Sep 26 10:33:48 CEST 2013
-        nowstr = datetime.strftime(datetime.now(),'%a %b %d %H:%M:%S %Z %Y')
-        f.write('#'+nowstr+'\n')
-        f.write('startdate='+self.startdate+'\n')
-        f.write('enddate='+self.enddate+'\n')
-        f.write('TELESCOPE='+self.telescope+'\n')
-        f.write('withfocus='+str(self.withfocus).lower()+'\n')
-        f.write('withacquire='+str(self.withacquire).lower()+'\n')
-        f.write('withguiding='+str(self.withguiding).lower()+'\n')
-        f.write('title='+str(self.title)+'\n')
-        f.write('uname='+str(self.uname)+'\n')
-        f.write('propid='+self.propid+'\n')
-        f.write('abstract='+self.abstract+'\n')
-        f.write('pi='+self.pi+'\n')
-        f.write('affil='+self.affil+'\n')
-        f.write('team='+self.team+'\n')
-        f.write('mode='+self.mode['mode']+'\n')
+        f.write('#%s\n' % datetime.strftime(datetime.now(),'%a %b %d %H:%M:%S %Z %Y'))
+        f.write('startdate=%s\n' % self.startdate)
+        f.write('enddate=%s\n' % self.enddate)
+        f.write('TELESCOPE=%s\n' % self.telescope)
+        f.write('withfocus=%s\n' % bin(self.withfocus))
+        f.write('withacquire=%s\n' % bin(self.withacquire))
+        f.write('withguiding=%s\n' % bin(self.withguiding))
+        f.write('title=%s\n' % str(self.title))
+        f.write('uname=%s\n' % str(self.uname))
+        f.write('propid=%s\n' % self.propid)
+        f.write('abstract=%s\n' % self.abstract)
+        f.write('pi=%s\n' % self.pi)
+        f.write('affil=%s\n' % self.affil)
+        f.write('team=%s\n' % self.team)
+        f.write('mode=%s\n' % self.mode['mode'])
         f.write('mode.timeout=%d\n' % self.mode['timeout'])
         f.write('mode.pernight=%d\n' % self.mode['pernight'])
         f.write('mode.impact=%.1f\n' % self.mode['impact'])
-        f.write('camera='+self.camera['camera']+'\n')
+        f.write('camera=%s\n' % self.camera['camera'])
         f.write('camera.XOffCCD=%d\n' % self.camera['XOffCCD'])
         f.write('camera.YOffCCD=%d\n' % self.camera['YOffCCD'])
         f.write('camera.XSizeCCD=%d\n' % self.camera['XSizeCCD'])
         f.write('camera.YSizeCCD=%d\n' % self.camera['YSizeCCD'])
         f.write('camera.XBinCCD=%d\n' % self.camera['XBinCCD'])
         f.write('camera.YBinCCD=%d\n' % self.camera['YBinCCD'])
-        f.write('sequence='+self.sequence['sequence']+'\n')
+        f.write('sequence=%s\n' % self.sequence['sequence'])
         f.write('sequence.ExposureTime=%.1f\n' % self.sequence['ExposureTime'])
         f.write('sequence.ExposureRepeat=%d\n' % self.sequence['ExposureRepeat'])
         f.write('sequence.ExposureIncrease=%s\n' % self.sequence['ExposureIncrease'])
@@ -149,14 +262,122 @@ class OpenCluster(object):
         f.write('constraints.AltTarget.Min=%d\n' % self.constraints['AltTarget.Min'])
         f.write('file=%s\n' % self.file)
         f.write('duration=%d\n' % self.duration)
+        f.flush()
         f.close()
-    
+
+        fin = open(self.filename, 'rt')
+        lines = fin.readlines()
+        fin.close()
+
     def fromfile(self):
-        pass    
+        """
+        loads the data from a save file
+        """
+        f = open(filename, 'rt')
+        lines = f.readlines()
+        f.close
+        #TODO: processing of lines
+        
+    def transfer(self, target='sro@stella:/stella/home/www/uploads/weingrill/save/'):
+        """
+        uploads the files to stella for the submission tool
+        """
+        
+        from subprocess import call
+        import os
+        source = self.filename
+        call(['/usr/bin/scp', source, target])
+
+    def tycho(self):
+        """
+        plot tycho stars upon fov
+        """
+        from tycho import tycho
+        
+        tycho(self.object['RA'], 
+              self.object['Dec'],
+              fov = 0.2*1.4142, 
+              grid=False, 
+              background=False, 
+              show=False)
+
+def do_ngc6940():
+    import time
+
+    ngc6940 = OpenCluster(objectname='NGC 6940', uname='NGC 6940 rot', obsmode='rot') #1850
+    ngc6940.title = 'TBD'
+    ngc6940.abstract = 'TBD'
+    ngc6940_subframes = ngc6940.plan_wifsip(nfields=4)
+    for sf in ngc6940_subframes:
+        print sf.uname
+        sf.plot()
+        sf.tycho()
+        time.sleep(1) # otherwise submit.jnlp gets confused
+        sf.tofile('/home/jwe/')
+        sf.transfer()
+    xmin,xmax = plt.xlim()
+    plt.xlim(xmax,xmin)
+    plt.show()
+
+def do_ngc2281():
+    import time
+
+    ngc2281 = OpenCluster(objectname='NGC 2281', uname='NGC 2281 rot', obsmode='rot')           
+    ngc2281_subframes = ngc2281.plan_wifsip(nfields=5)
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    for sf in ngc2281_subframes:
+        print sf.uname
+        sf.plot()
+        sf.tycho()
+        time.sleep(1) # otherwise submit.jnlp gets confused
+        sf.tofile('/home/jwe/')
+        sf.transfer()
+    xmin,xmax = plt.xlim()
+    plt.xlim(xmax,xmin)
+    plt.show()
+
+def do_ngc1674():
+    import time
+
+    ngc2281 = OpenCluster(objectname='NGC 1647', uname='NGC 1647 bv', obsmode='bv')           
+    ngc2281_subframes = ngc2281.plan_wifsip(nfields=5)
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_aspect(1.)
+
+    for sf in ngc2281_subframes:
+        print sf.uname
+        sf.plot()
+        sf.tycho()
+        time.sleep(1) # otherwise submit.jnlp gets confused
+        sf.tofile('/home/jwe/')
+        sf.transfer()
+    xmin,xmax = plt.xlim()
+    plt.xlim(xmax,xmin)
+    plt.show()
+
+def do_ngc2281center():
+    import time
+
+    ngc2281 = OpenCluster(objectname='NGC 2281', uname='NGC 2281 BV center', obsmode='bvsl20')           
+    ngc2281.plot()
+    ngc2281.tycho()
+    time.sleep(1) # otherwise submit.jnlp gets confused
+    ngc2281.tofile('/home/jwe/')
+    ngc2281.transfer()
+    xmin,xmax = plt.xlim()
+    plt.xlim(xmax,xmin)
+    plt.show()
         
 if __name__ == '__main__':
-    ngc2281 = OpenCluster(objectname='NGC 2281', uname='NGC2281 SW BVI') #1850
-    ngc2281.get_coordiantes()
-    ngc2281.plan_wifsip(nfields=4)
-    ngc2281.tofile('/home/jwe/')
+    import matplotlib
+    matplotlib.use('WXAgg')
+    import matplotlib.pyplot as plt
+    #do_ngc2281center()
+    do_ngc1674()    
+    
     
---
+++ src/tycho.py
@@ -0,0 +1,42 @@
+'''
+Created on Oct 17, 2013
+
+@author: jwe
+'''
+def tycho(ra_center, dec_center, fov = 1.0, grid=True, background=True, show=True):
+    import matplotlib.pyplot as plt
+
+    import pylab
+    from functions import scaleto
+    from numpy import clip, array
+    from datasource import DataSource
+    
+    tycho = DataSource(database='wifsip', host='pina', user='sro')
+    query = """select mradeg, mdedeg, vt, bt-vt
+         from tycho where circle(point(%f,%f),%f) @> circle(coord,0)
+         and not bt-vt is null;""" % (ra_center, dec_center, fov)
+    data = tycho.query(query)
+    ra = array([float(d[0]) for d in data])
+    dec = array([float(d[1]) for d in data])
+    vmag = array([float(d[2]) for d in data])
+    bv = array([float(d[3]) for d in data])
+    tycho.close()
+        
+    bv = clip(bv,-0.3,1.52) # B0 to M4
+    
+    pts = scaleto(vmag,[80.0,1.0])
+    #print min(vmag),max(vmag),len(vmag)
+    if background:
+        plt.subplot(111, axisbg='darkblue')
+    #Plejades, Hyades 03 47 00 +24 07.0
+    if grid:
+        plt.grid(color='w')
+    plt.scatter(ra, dec)
+    plt.scatter(ra, dec, s=pts, c=-bv, edgecolor='none', cmap=pylab.cm.Spectral)
+    if show:
+        plt.show()
+    
+if __name__ == '__main__':
+    import matplotlib
+    matplotlib.use('WXAgg')
+    tycho(308.6083,28.2833)
