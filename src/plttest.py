812a3ffca30ad9109cd72d38a41761c7511b0564
Joerg Weingrill <jweingrill@aip.de>
2013-11-05 09:42:12 +0100

    plttest: new tickmarks and new demo function

--- src/plttest.py
+++ src/plttest.py
@@ -9,17 +9,18 @@ from astronomy import airmass, height
 import matplotlib
 matplotlib.use('WXAgg')
 import matplotlib.pyplot as plt
-from numpy import linspace, pi, arcsin, array
+from numpy import linspace, pi, sin, array
 
 def update_ax2(ax1):
     y1, y2 = ax1.get_ylim()
-    yticks = [1.0,1.1,1.3,1.5,2.0,2.5]
-    ax2.set_yticks(yticks)
+    #ax2.set_yticks(yticks)
     ax2.set_ylim(airmass(y1), airmass(y2))
     ax2.figure.canvas.draw()
 
 
 a = 3.0
+print 'the height of airmass %.1f is %.2f' % (a, height(a))
+a = 5.0
 print 'the height of airmass %.1f is %.2f' % (a, height(a))
 
 fig, ax1 = plt.subplots()  # ax1 is the height scale
@@ -27,20 +28,24 @@ ax2 = ax1.twinx()          # ax2 is the airmass scale
 
 
 # automatically update ylim of ax2 when ylim of ax1 changes.
-ax1.callbacks.connect("ylim_changed", update_ax2)
+#ax1.callbacks.connect("ylim_changed", update_ax2)
 t = linspace(0, 1.0)
-y = 90.0*t**2.0
+#y = 90.0*(1.0-(t-0.5)**2.0)
+y = -360.*t**2 + 360.*t
+y1 = 90.*sin(t*pi)
 ax1.plot(t, y)
+ax1.plot(t, y1, 'g')
+
+airmasses = array([1.0,1.1,1.2,1.3,1.4,1.5,2.0,3.0,4.0,5.0])
+heights = [height(am) for am in airmasses]
+yticks = [height(am) for am in airmasses]
+yticklabels = [str(round(am,1)) for am in airmasses]
+ax2.set_yticks(yticks)
+ax2.set_yticklabels(yticklabels)
 
 ax1.set_title('Two scales: height and airmass')
 ax1.set_ylabel('height')
 ax2.set_ylabel('airmass')
-#airmasses = array([1.0,1.1,1.3,1.5,2.0,2.5,2.8])
-#heights = [height(am) for am in airmasses]
-#yticks = [height(am) for am in airmasses]
-#yticklabels = [str(round(am,1)) for am in airmasses]
-#ax2.set_yticks(yticks)
-#ax2.set_yticklabels(yticklabels)
 plt.show()
 
 """
