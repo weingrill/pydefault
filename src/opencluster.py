e92d91a75d1bfe2038e227a8c5c42b58dcf8d726
hg <devnull@localhost>
2013-11-14 12:17:45 +0100

    Changes de to 'BlockedPerNight' in opencluster

--- src/opencluster.py
+++ src/opencluster.py
@@ -15,15 +15,15 @@ class OpenCluster(object):
                     or 'cmd' for BVI photometry
         """
         self.startdate = '2013-03-21'
-        self.enddate = '2014-12-31'
+        self.enddate = '2018-12-31'
         self.telescope = 'STELLA-I'
         self.withfocus = True
         self.withacquire = True
         self.withguiding = True
-        self.title = ''
+        self.title = 'SOCS'
         self.uname = uname
         self.propid = 'cluster.survey'
-        self.abstract = ''
+        self.abstract = 'Photometric monitoring of open stellar clusters'
         self.pi = 'Weingrill'
         self.affil = 'CORE'
         self.team = 'Barnes'
@@ -47,7 +47,7 @@ class OpenCluster(object):
         self.object = {'ObjectName':objectname,
                        'RA':ra,
                        'Dec':dec}
-        self.constraints = {'MoonDistance.Min':45,
+        self.constraints = {'MoonDistance.Min':20,
                             'SolHeight.Max':-16.0,
                             'AirmassTarget.Max':2.0,
                             'AltTarget.Min':30.0}
@@ -56,62 +56,63 @@ class OpenCluster(object):
         self._duration = 0
         self.fields = 1
 
-        if obsmode == 'bvs':
-            self.sequence['ExposureTime'] = 30.0
-            self.sequence['ExposureRepeat'] = 9
-            self.sequence['ExposureIncrease'] = '3*1,3*3'
-            self.sequence['FilterSequence'] = '3*V,3*B'
-            self.mode['pernight'] = 2 # can be refined
-
-        if obsmode == 'bvl':
-            self.sequence['ExposureTime'] = 300.0
-            self.sequence['ExposureRepeat'] = 9
-            self.sequence['ExposureIncrease'] = '3*1,3*3'
-            self.sequence['FilterSequence'] = '3*V,3*B'
-            self.mode['pernight'] = 2 # can be refined
-        
         if obsmode == 'bv':
             self.sequence['ExposureTime'] = 30.0
             self.sequence['ExposureRepeat'] = 12
             self.sequence['ExposureIncrease'] = '3*1,3*3,3*10,3*10'
             self.sequence['FilterSequence'] = '3*V,3*B,3*V,3*B'
-            self.mode['pernight'] = 2 # can be refined
+            self.mode['mode'] = 'BlockedPerNight'
+            self.mode['maxobservations'] = 20
+            self.mode['pernight'] = 2 
+            self.mode['timeout'] = 3600000
+            self.mode['jd0'] = 2456609.0
+            self.mode['zero'] = 180.0
 
         if obsmode == 'bvsl20':
             self.sequence['ExposureTime'] = 20.0 # 20.0 + 200.0
             self.sequence['ExposureRepeat'] = 20
             self.sequence['ExposureIncrease'] = '5*1,5*3,5*10,5*10'
             self.sequence['FilterSequence'] = '5*V,5*B,5*V,5*B'
-            self.mode['pernight'] = 2 # can be refined
+            self.mode['mode'] = 'BlockedPerNight'
+            self.mode['maxobservations'] = 20
+            self.mode['pernight'] = 2 
+            self.mode['timeout'] = 3600000
+            self.mode['jd0'] = 2456609.0
+            self.mode['zero'] = 180.0
 
         if obsmode == 'bvsl30':
-            self.sequence['ExposureTime'] = 30.0 # 20.0 + 200.0
+            self.sequence['ExposureTime'] = 30.0 # 30.0 + 300.0
             self.sequence['ExposureRepeat'] = 20
             self.sequence['ExposureIncrease'] = '5*1,5*3,5*10,5*10'
             self.sequence['FilterSequence'] = '5*V,5*B,5*V,5*B'
-            self.mode['pernight'] = 2 # can be refined
+            self.mode['mode'] = 'BlockedPerNight'
+            self.mode['maxobservations'] = 20
+            self.mode['pernight'] = 2 
+            self.mode['timeout'] = 3600000
+            self.mode['jd0'] = 2456609.0
+            self.mode['zero'] = 180.0
 
-        
-        if obsmode == 'cmds':
-            self.sequence['ExposureTime'] = 30.0
-            self.sequence['ExposureRepeat'] = 9
-            self.sequence['ExposureIncrease'] = '3*1,3*1.5,3*3'
-            self.sequence['FilterSequence'] = '3*I,3*V,3*B'
-            self.mode['pernight'] = 2 # can be refined
-            
-        if obsmode == 'cmdl':
-            self.sequence['ExposureTime'] = 300.0
-            self.sequence['ExposureRepeat'] = 9
-            self.sequence['ExposureIncrease'] = '3*1,3*1.5,3*3'
-            self.sequence['FilterSequence'] = '3*I,3*V,3*B'
-            self.mode['pernight'] = 2 # can be refined
+        if obsmode == 'bvi':
+            self.sequence['ExposureTime'] = 200.0 
+            self.sequence['ExposureRepeat'] = 15
+            self.sequence['ExposureIncrease'] = '5*1,5*1.5,5*3'
+            self.sequence['FilterSequence'] = '5*I,5*V,5*B'
+            self.mode['mode'] = 'BlockedPerNight'
+            self.mode['maxobservations'] = 20
+            self.mode['pernight'] = 2 
+            self.mode['timeout'] = 3600000
+            self.mode['jd0'] = 2456609.0
+            self.mode['zero'] = 180.0
 
         if obsmode == 'rot':
             self.sequence['ExposureTime'] = 30.0
+            self.sequence['ExposureRepeat'] = 2
             self.sequence['ExposureIncrease'] = '1,10'
             self.sequence['FilterSequence'] = 'V,V'
+            self.mode['mode'] = 'FewPerNight'
             self.mode['timeout'] = 1320000 # duration*fields*1000
             self.mode['pernight'] = 10 # can be refined
+            self.mode['impact'] = 0.5
         
         
         
@@ -141,7 +142,7 @@ class OpenCluster(object):
         """
         returns new subframes
         """
-        d = 1320.2/3600.0
+        d = 1320.2/3600.0 # 1320.2arcsec is the fov of WiFSIP
         d2 = d/2
         cra, cdec = self.object['RA'],self.object['Dec']
         if nfields == 4:
@@ -164,15 +165,31 @@ class OpenCluster(object):
             subframes.append(OpenCluster(objectname=self.object['ObjectName'],
                                          uname = '%s %s' % (self.uname, names[i]), 
                                          ra = f[0], dec = f[1]))
+        #do some deep copy of the object:
         for sf in subframes:
+            sf.startdate = self.startdate
+            sf.enddate = self.enddate
+            sf.telescope = self.telescope
+            sf.withfocus = self.withfocus
+            sf.withacquire = self.withacquire
+            sf.withguiding = self.withguiding
             sf.title = self.title
+            #sf.uname = self.uname # is set at init
+            sf.propid = self.propid
             sf.abstract = self.abstract
+            sf.pi = self.pi
+            sf.affil = self.affil
+            sf.team = self.team
+            sf.title = self.title
             sf.mode = self.mode
+            sf.camera = self.camera
             sf.sequence = self.sequence
-        
+            #sf.object = self.object # new coordinates and object mustn't change!
+            sf.constraints = self.constraints
+            
         return subframes
             
-    def plot(self):
+    def plot(self, axis=None):
         """
         plot the fov
         """
@@ -181,7 +198,10 @@ class OpenCluster(object):
         d2 = 0.5*1320.2/3600.0
         ras = [ra-d2, ra+d2, ra+d2, ra-d2, ra-d2]
         das = [dec-d2, dec-d2, dec+d2, dec+d2, dec-d2]
-        plt.plot(ras, das)
+        if axis is None:
+            plt.plot(ras, das)
+        else:
+            axis.plot(ras, das)
 
     @property
     def duration(self):
@@ -237,9 +257,18 @@ class OpenCluster(object):
         f.write('affil=%s\n' % self.affil)
         f.write('team=%s\n' % self.team)
         f.write('mode=%s\n' % self.mode['mode'])
-        f.write('mode.timeout=%d\n' % self.mode['timeout'])
-        f.write('mode.pernight=%d\n' % self.mode['pernight'])
-        f.write('mode.impact=%.1f\n' % self.mode['impact'])
+        if self.mode['mode']=='FewPerNight':
+            f.write('mode.timeout=%d\n' % self.mode['timeout'])
+            f.write('mode.pernight=%d\n' % self.mode['pernight'])
+            f.write('mode.impact=%.1f\n' % self.mode['impact'])
+            
+        if self.mode['mode']=='BlockedPerNight':
+            f.write('mode.maxobservations=%d\n' % self.mode['maxobservations'])
+            f.write('mode.pernight=%d\n' % self.mode['pernight'])
+            f.write('mode.timeout=%d\n' % self.mode['timeout'])
+            f.write('mode.jd0=%.1f\n' % self.mode['jd0'])
+            f.write('mode.zero=%.1f\n' % self.mode['zero'])
+            
         f.write('camera=%s\n' % self.camera['camera'])
         f.write('camera.XOffCCD=%d\n' % self.camera['XOffCCD'])
         f.write('camera.YOffCCD=%d\n' % self.camera['YOffCCD'])
@@ -248,7 +277,7 @@ class OpenCluster(object):
         f.write('camera.XBinCCD=%d\n' % self.camera['XBinCCD'])
         f.write('camera.YBinCCD=%d\n' % self.camera['YBinCCD'])
         f.write('sequence=%s\n' % self.sequence['sequence'])
-        f.write('sequence.ExposureTime=%.1f\n' % self.sequence['ExposureTime'])
+        f.write('sequence.ExposureTime=%d\n' % self.sequence['ExposureTime'])
         f.write('sequence.ExposureRepeat=%d\n' % self.sequence['ExposureRepeat'])
         f.write('sequence.ExposureIncrease=%s\n' % self.sequence['ExposureIncrease'])
         f.write('sequence.FilterSequence=%s\n' % self.sequence['FilterSequence'])
@@ -257,9 +286,9 @@ class OpenCluster(object):
         f.write('object.RA=%.5f\n' % self.object['RA'])
         f.write('object.Dec=%.6f\n' % self.object['Dec'])
         f.write('constraints.MoonDistance.Min=%d\n' % self.constraints['MoonDistance.Min'])
-        f.write('constraints.SolHeight.Max=%d\n' % self.constraints['SolHeight.Max'])
-        f.write('constraints.AirmassTarget.Max=%d\n' % self.constraints['AirmassTarget.Max'])
-        f.write('constraints.AltTarget.Min=%d\n' % self.constraints['AltTarget.Min'])
+        f.write('constraints.SolHeight.Max=%.1f\n' % self.constraints['SolHeight.Max'])
+        f.write('constraints.AirmassTarget.Max=%.1f\n' % self.constraints['AirmassTarget.Max'])
+        f.write('constraints.AltTarget.Min=%.1f\n' % self.constraints['AltTarget.Min'])
         f.write('file=%s\n' % self.file)
         f.write('duration=%d\n' % self.duration)
         f.flush()
@@ -301,24 +330,6 @@ class OpenCluster(object):
               background=False, 
               show=False)
 
-def do_ngc6940():
-    import time
-
-    ngc6940 = OpenCluster(objectname='NGC 6940', uname='NGC 6940 rot', obsmode='rot') #1850
-    ngc6940.title = 'TBD'
-    ngc6940.abstract = 'TBD'
-    ngc6940_subframes = ngc6940.plan_wifsip(nfields=4)
-    for sf in ngc6940_subframes:
-        print sf.uname
-        sf.plot()
-        sf.tycho()
-        time.sleep(1) # otherwise submit.jnlp gets confused
-        sf.tofile('/home/jwe/')
-        sf.transfer()
-    xmin,xmax = plt.xlim()
-    plt.xlim(xmax,xmin)
-    plt.show()
-
 def do_ngc2281():
     import time
 
@@ -351,13 +362,13 @@ def do_ngc1674():
 
     for sf in ngc2281_subframes:
         print sf.uname
-        sf.plot()
+        sf.plot(ax)
         sf.tycho()
         time.sleep(1) # otherwise submit.jnlp gets confused
         sf.tofile('/home/jwe/')
         sf.transfer()
     xmin,xmax = plt.xlim()
-    plt.xlim(xmax,xmin)
+    ax.set_xlim(xmax,xmin)
     plt.show()
 
 def do_ngc2281center():
