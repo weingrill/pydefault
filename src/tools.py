3236771792bfdda4cf59ae8dfa596e10ccf28547
hg <devnull@localhost>
2014-03-17 14:42:42 +0100

    removed log; added log to tools

--- src/tools.py
+++ src/tools.py
@@ -27,3 +27,21 @@ class FileCache(str):
         self.cachefile = cachefile
         return self.cachefile
     
+def log(logfile, logstring='', silent=False):
+    '''
+    Created on Jan 14, 2013
+
+    @author: jwe
+    Prints logstring to a logfile
+    '''
+    from datetime import datetime
+
+    now = datetime.now()
+    try:
+        f = open(logfile,'at')
+        f.write('%s\t%s\n' % (now, logstring.rstrip('\n')))
+        f.close()
+    except IOError:
+        print 'Can''t write logfile %s!' % logfile
+    if not silent:
+        print logstring    
