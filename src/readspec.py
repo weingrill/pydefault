339aa9e365bbe64fdc73192fa911f6ffaeadd226
Joerg Weingrill <jweingrill@aip.de>
2014-10-20 19:31:53 +0200

    tried pyspeckit without success

--- src/readspec.py
+++ src/readspec.py
@@ -3,9 +3,9 @@ Created on 17.04.2013
 
 @author: jwe
 '''
+import numpy as np
 class Spectrum(object):
     def __init__(self):
-        import numpy as np
         mu, sigma = 1.0, 0.01 # mean and standard deviation
         r = 1000
         self.intensity = np.random.normal(mu, sigma, r)
@@ -24,10 +24,16 @@ class Spectrum(object):
     def fromfile(self, filename):
         import pyfits
     
-        filename = '/Users/jwe/Dropbox/Public/ses1/science20130408B-0030_botzfxsEcd.fits'
+        filename = '/work1/jwe/Dropbox/Public/ses1/science20130410B-0008_botzfxsEcd.fits'
         hdulist = pyfits.open(filename)
         hdr = hdulist[0].header
-        stars = hdulist[1].data
+        data = hdulist[0].data
+        self.spectrum = data[0,40]
+        self.raw = data[1,40]
+        self.sigma = data[2,40]
+        self.intensity = data[1,40]
+        self.wavelength = np.arange(len(self.intensity))
+        print self.wavelength.shape
         hdulist.close()
     
     def continuum(self, width):   
@@ -45,18 +51,52 @@ class Spectrum(object):
         self.cont = array(c)
         
     def plot(self):
-        import matplotlib
-
-        matplotlib.use('WXAgg')
         import matplotlib.pyplot as plt
-        plt.scatter(self.wavelength,self.intensity)
-        plt.plot(self.wavelength,self.cont,'r')
+        #plt.scatter(self.wavelength,self.intensity)
+        plt.plot(self.spectrum,'r')
+        plt.plot(self.raw,'b')
         plt.show()
         
 if __name__ == '__main__':
+    import pyspeckit
+    from pylab import *
+    #import wav2rgb
     
+    speclist = pyspeckit.wrappers.load_IRAF_multispec('/work1/jwe/Dropbox/Public/ses1/science20130410B-0008_botzfxsEcd.fits')
+    
+    for spec in speclist:
+        spec.units="Counts"
+    
+    SP = pyspeckit.Spectra(speclist)
+    SPa = pyspeckit.Spectra(speclist,xunits='angstroms',quiet=False)
+    
+    SP.plotter(figure=figure(1))
+    SPa.plotter(figure=figure(2))
+    
+    figure(3)
+    clf()
+    figure(4)
+    clf()
+    
+    clr = [list(clr) for clr in matplotlib.cm.brg(linspace(0,1,len(speclist)))]
+    #clr = [wav2rgb.wav2RGB(c) + [1.0] for c in linspace(380,780,len(speclist))][::-1]
+    for ii,(color,spec) in enumerate(zip(clr,speclist)):
+        spec.plotter(figure=figure(3), clear=False, reset=False, color=color, refresh=False)
+    
+        fig4=figure(4)
+        fig4.subplots_adjust(hspace=0.35,top=0.97,bottom=0.03)
+        spec.plotter(axis=subplot(10,1,ii%10+1), clear=False, reset=False, color=color, refresh=False)
+        spec.plotter.axis.yaxis.set_major_locator( matplotlib.ticker.MaxNLocator(4) )
+    
+        if ii % 10 == 9:
+            spec.plotter.refresh()
+            spec.plotter.savefig('vega_subplots_%03i.png' % (ii/10+1))
+            clf()
+    
+    spec.plotter.refresh()    
 
     f = '/Users/jwe/Dropbox/Public/ses1/science20130408B-0030_botzfxsEcd.fits'
     sp = Spectrum()
-    sp.continuum(200)
+    #sp.continuum(200)
+    sp.fromfile(f)
     sp.plot()
