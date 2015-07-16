d67cd2d12576b1e4539511e6c259115d4703df6d
Joerg Weingrill <jweingrill@aip.de>
2015-07-16 14:52:58 +0200

    improvements in record and added examples accordingly

--- src/record.py
+++ src/record.py
@@ -33,7 +33,7 @@ class Record(object):
         if type(newrecord)==dict:
             self.table.append(newrecord)
             
-        if type(newrecord)==list:
+        elif type(newrecord)==list:
             if columns is None:
                 columns = self.columns
             for record in newrecord:
@@ -41,6 +41,8 @@ class Record(object):
                 for i in range(len(record)):
                     recdict[columns[i]] = record[i]
                 self.table.append(recdict)
+        else:
+            raise(TypeError)
     
     def __getitem__(self, keyvalue):
         """
@@ -51,9 +53,15 @@ class Record(object):
           
         """
         if type(keyvalue) == str:
+            # try to find the key with keyvalue
             for row in self.table:
                 if row[self.key] == keyvalue:
                     return row
+            if keyvalue in self.columns:
+                return [row[keyvalue] for row in self.table]
+            else:
+                raise(ValueError)
+                    
         elif type(keyvalue) == int or type(keyvalue) == slice:
             return self.table[keyvalue]
         
@@ -63,21 +71,42 @@ class Record(object):
         else:
             print 'unknown type ', type(keyvalue)
             
+    def __len__(self):
+        return len(self.table)
+    
+    def keys(self):
+        return self.columns
+    
+    def __call__(self, **kwargs):
+        key, value = kwargs.items()[0]
+        return [row for row in self.table if row[key] == value]
+
+    def __str__(self):
+        s = ''
+        s += '\t'.join(self.table[0].keys()) + '\n'
+        for row in self.table:
+            s += '\t'.join([str(row[c]) for c in self.table[0].keys()]) + '\n'
+        return s
+        
 if __name__ == '__main__':
-    r = Record(key = 'name', columns=['name','email','salary'])
-    r.append({'name':'Joerg', 'email':'joerg@weingrill.net', 'salary': 2500.0})
-    r.append({'name':'Katja', 'email':'kjanssen@aip.de', 'salary': 2800.0})
-    r.append({'name':'Trey', 'email':'cmack@aip.de', 'salary': 2300.0})
-    print r['Joerg']
-    print r['Katja']
-    print r[:,'salary']
-    print r[1:3]
-    print r[1:3,'salary']
+    r1 = Record(key = 'name', columns=['name','email','salary','location'])
+    r1.append({'name':'Joerg', 'email':'joerg@weingrill.net', 'salary': 2500.0, 'location': 'Potsdam'})
+    r1.append({'name':'Katja', 'email':'kjanssen@aip.de', 'salary': 2800.0, 'location': 'Potsdam'})
+    r1.append({'name':'Trey', 'email':'cmack@aip.de', 'salary': 2300.0, 'location': 'Berlin'})
+    print r1['Joerg']
+    print r1['Katja']
+    print r1[:,'salary']
+    print r1['salary']
+    print 'Who is in Potsdam?', r1(location = 'Potsdam')
+    #print r1['Susy']
+    
+    print r1[1:3]
+    print r1[1:3,'salary']
+    print r1
     exit()
     r = Record(key = 'objid', columns=['objid','path','filename', 'fwhm', 'zero'])
     import psycopg2
 
-    pipe_ver = '3.6'
     
     query = """(SELECT objid, path, filename, fwhm, zero 
         FROM science
@@ -86,9 +115,8 @@ if __name__ == '__main__':
         (SELECT science.objid, path, filename, fwhm, zero 
         FROM science, frames
         WHERE science.objid=frames.objid 
-        AND frames.pipe_ver<'%s')
-        ORDER BY objid DESC 
-        LIMIT 200;""" % pipe_ver
+        ORDER BY objid DESC) 
+        LIMIT 200;"""
      
     wifsip = psycopg2.connect(database='stella', user='stella', host='pera.aip.de')
     cur = wifsip.cursor()
