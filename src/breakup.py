4f0c11607ba9e1acdfe9e12dfa4eb75512759608
Joerg Weingrill <jweingrill@aip.de>
2015-07-31 14:56:26 +0200

    breakup now working correctly

--- src/breakup.py
+++ src/breakup.py
@@ -7,33 +7,54 @@ Created on Jul 30, 2015
 
 calculate the breakup as a function of mass or B-V respectively
 '''
-from numpy import sqrt, loadtxt, pi
-iso = loadtxt('/home/jwe/data/iso_01.000_Gyr.dat')
-#print iso[:,0]
+def breakup():
+    
+    from numpy import sqrt, loadtxt, pi, argmin, arange
+    iso = loadtxt('/home/jwe/data/iso_01.000_Gyr.dat')
+    #print iso[:,0]
+    
+    
+    mass = iso[:,0]
+    radius = iso[:,3]
+    bv = iso[:,8]
+    logg = iso[:,4]
+    
+    g = 10**(logg-2) #convert from logg [gcms] to g [kms]
+    
+    
+    minbv = argmin(bv)
+    i = arange(minbv)
+    
+    G = 6.673e-11
+    bv = bv[i]
+    radius = radius[i]
+    mass = mass[i]
+    g = g[i]
+    
+    
+    R_sun = 695660e3
+    r = 10**radius * R_sun
+    M_sun = 1.98855e30
+    GM_sun = 1.32712442099e20
+    
+    omega = sqrt(GM_sun*mass / r**3)
+    
+    P = sqrt(4.0*r*pi**2/g)/86400
+    Po = (2.0*pi/omega)/86400
+    return bv, P
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+    bv, P = breakup()
+#plt.plot(bv, omega, 'r', label='$\Omega$')
+    plt.plot(bv, P,     'g', label='P')
+#plt.plot(bv, Po,    'b', label='P$_o$')
 
+#plt.xlabel('B - V')
+#plt.ylabel('P_$breakup$ [days]')
+    plt.legend()
+    plt.show()
+#print bv
+#print P
 
-mass = iso[:,0]
-radius = iso[:,3]
-bv = iso[:,8]
-g = iso[:,8]
-
-G = 6.673e-11
-bv = bv[mass<2.0]
-radius = radius[mass<2.0]
-mass = mass[mass<2.0]
-g = g[mass<2.0]
-
-R_sun = 695660e3
-r = radius * R_sun
-M_sun = 1.98855e30
-GM_sun = 1.32712442099e20
-from matplotlib import pyplot as plt
-
-omega = sqrt(GM_sun*mass / r**3)
-
-#P = sqrt(4.0*r*pi**2/g)/86400
-P = (2.0*pi/omega)/86400
-plt.plot(bv, P)
-plt.show()
-#omega = G * M / r**3
-print omega
+#for mi,ri,bvi,gi, ppi in zip(mass, radius, bv, logg, P):
+#    print '%.2f %.2f %.2f %.2f %.2f' % (mi,ri,bvi,gi, ppi)
