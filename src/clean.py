87cd97dc2dd77c64638ad02d6c5adab65cc49139
Joerg Weingrill <jweingrill@aip.de>
2015-08-20 13:07:26 +0200

    clean: clean now renamed to clean_crane clean: gain and threshold introduced filename now comes from glob changes in plots

--- src/clean.py
+++ src/clean.py
@@ -54,7 +54,10 @@ def window(t, x):
     assert( X.shape == f.shape)
     return X, f
 
-def clean(t, x, g = 0.2):
+def clean_crane(t, x, g = 0.2):
+    """
+    from Patrick C Crane: Solar Physics 203: 381-408, 2001
+    """
     N = np.shape(t)[0]
     x -= np.mean(x)
     dtmin = np.min(abs(t - np.roll(t,1)))
@@ -133,7 +136,7 @@ def clean(t, x, g = 0.2):
         
     return c, f
 
-def clean1(t, x):
+def clean(t, x, gain = 0.5, threshold = 1e-9):
     x -= np.mean(x)
     t -= t[0]
     ft, f = dft(t, x)
@@ -142,67 +145,83 @@ def clean1(t, x):
     cleaned = np.zeros(len(ft))
     n2 = len(ft) / 2
 
-    for k in range(100):
+    for k in range(1000):
         i = np.argmax(aft[n2:])
         ipos = i + n2
         ineg = n2 - i
-        gain = aft[ipos]
+        amp = gain*aft[ipos]
         sigma = np.std(aft[n2:])
-        if gain < sigma: break
-        print k, gain, sigma
+        if amp < sigma or amp < threshold: break
+        print k, i, amp, sigma
         
-        cleaned[ipos] += gain
-        cleaned[ineg] += gain
+        cleaned[ipos] += amp
+        cleaned[ineg] += amp
         pftw = np.roll(abs(ftw), i)[n2:len(ft)+n2]
         pftw += np.roll(abs(ftw), -i)[n2:len(ft)+n2]
 #        plt.plot(f,pftw)
-        aft = abs(aft - gain*pftw)
+        aft = abs(aft - amp*pftw)
     
     from scipy import signal
-    gauss = signal.gaussian(len(cleaned), 1.0,)
+    gauss = signal.gaussian(len(cleaned), 1.5)
     cleaned = signal.convolve(cleaned, gauss, mode='same')
     return f, cleaned, aft
 
-
 if __name__ == '__main__':
-    filename = '/work2/jwe/SOCS/M48/lightcurves.new/20140403A-0079-0002#1499.dat'
+    from glob import glob
+    filenames = glob('/work2/jwe/SOCS/M48/lightcurves.new/*.dat')
+    filename = filenames[47]
     t, x = np.loadtxt(filename, unpack=True)
 
     #t = np.linspace(0,63, 100)
-    p = 7.2357
-    #x = 0.3*np.sin(2.*np.pi*t/p)+0.2
-    #x += np.random.normal(scale=0.3, size=x.shape)
+    p0 = 17.2357
+    x = 0.3*np.sin(2.*np.pi*t/p0)+0.2
+    x += np.random.normal(scale=0.3, size=x.shape)
     #from random import shuffle
     #shuffle(x)
     
     plt.figure(figsize=(6,9))
-
     plt.subplot('511')
-    plt.plot(t, x, 'o-')
+    plt.title(filename[36:-4])
+    plt.plot(t, x, 'ko-')
     
     plt.subplot('512')
     ft, f = dft(t, x)
-    plt.plot(f, abs(ft))
+    plt.plot(f, abs(ft), 'k')
+    sigma = np.std(ft[f>=0])
     
     plt.subplot('513')
     ftw, fw = window(t, x)
-    plt.plot(fw, abs(ftw))
+    plt.plot(fw, abs(ftw), 'k')
     print 'ftw', len(ftw)
     
     aft = abs(ft)
 
-    f, cleaned, res = clean1(t, x)
+    f, cleaned, res = clean(t, x)
+    n2 = len(f) /2
+    cf = cleaned[n2+1:]/sigma
+    p = 1./f[n2+1:]
+    cf = cf[p>1.1]
+    p = p[p>1.1]
+    i = np.argmax(cf)
+    if cf[i]>1.0:
+        period = p[i]
+    else:
+        period = 0.0
     
     plt.subplot('514')
-    plt.plot(f, res)
+    plt.plot(f, res, 'k')
     
     plt.subplot('515')
 
-    n2 = len(f) /2
     plt.axvline(1, color='r')
-    plt.xlim(0.0,30.0)
-    plt.plot(1./f[n2+1:], cleaned[n2+1:])
+    plt.axvline(p0, color='b')
+    plt.axvline(period, color='g')
+    plt.xlim(0.0,max(t)/2)
+    plt.plot(p[cf>0.01], cf[cf>0.01], 'k')
+    if cf[i]>1.0:
+        plt.xlabel('period = %.1f' % period)
+        
     plt.savefig('clean.pdf')
     plt.close()
     
-    exit()
+    
