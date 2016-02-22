725b48f5291aa47abf530aad9c334a7f809e4f7d
Joerg Weingrill <jweingrill@aip.de>
2016-02-22 14:13:12 +0100

    fitsvale: cleaned up code

--- src/fitsvalue.py
+++ src/fitsvalue.py
@@ -3,9 +3,18 @@
 '''
 Created on Sep 27, 2013
 
-@author: jwe
+@author: Joerg Weingrill <jweingrill@aip.de>
 '''
-import pyfits
-import sys
-hdr = pyfits.getheader(sys.argv[1])
-print hdr[sys.argv[2]]
+
+if __name__ == '__main__':
+    import argparse
+    import astropy.io.fits as pyfits
+    
+    parser = argparse.ArgumentParser(description='reads a specific key of a FITS file')
+    parser.add_argument('-key', help='FITS header key')
+    parser.add_argument('filename', help='name of FITS file')
+
+    args = parser.parse_args()
+
+    hdr = pyfits.getheader(args.filename)
+    print hdr[args.key]
