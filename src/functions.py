d8b4b6ff11372e6cdcf95563787d5da13b264435
hg <devnull@localhost>
2018-04-13 14:54:36 +0200

    added regression function (untested)

--- src/functions.py
+++ src/functions.py
@@ -191,3 +191,55 @@ def largmin(array, i0, direction='left'):
 def runningmean(x, N=97):
     return np.convolve(x, np.ones((N,))/N, mode='same')
     
+def regression(x, y, x_err, y_err):
+    '''
+    source 
+    https://stackoverflow.com/questions/22670057/linear-fitting-in-python-with-uncertainty-in-both-x-and-y-coordinates
+    '''
+    from scipy.odr import Model, RealData, ODR
+    # Initiate some data, giving some randomness using random.random().
+    x = np.array(x)
+    y = np.array(y)
+    
+    x_err = np.array(x_err)
+    y_err = np.array(y_err)
+    
+    # Define a function (quadratic in our case) to fit the data with.
+    def lin_func(p, x):
+        k, d = p
+        return k*x + d
+    
+    # Create a model for fitting.
+    quad_model = Model(lin_func)
+    
+    # Create a RealData object using our initiated data from above.
+    data = RealData(x, y, sx=x_err, sy=y_err)
+    
+    # Set up ODR with the model and data.
+    odr = ODR(data, quad_model, beta0=[0., 1.])
+    
+    # Run the regression.
+    out = odr.run()
+    
+    # Use the in-built pprint method to give us results.
+    out.pprint()
+    '''Beta: [ 1.01781493  0.48498006]
+    Beta Std Error: [ 0.00390799  0.03660941]
+    Beta Covariance: [[ 0.00241322 -0.01420883]
+     [-0.01420883  0.21177597]]
+    Residual Variance: 0.00632861634898189
+    Inverse Condition #: 0.4195196193536024
+    Reason(s) for Halting:
+      Sum of squares convergence'''
+    
+    x_fit = np.linspace(x[0], x[-1], 1000)
+    y_fit = lin_func(out.beta, x_fit)
+    
+    #plt.errorbar(x, y, xerr=x_err, yerr=y_err, linestyle='None', marker='x')
+    #plt.plot(x_fit, y_fit)
+    
+    #plt.show()        
+    return out.beta
+
+
+
