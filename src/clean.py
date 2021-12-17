8161fce91fdd3721589fe1fdaa65844d3785f8de
Jörg Weingrill <jweingrill@aip.de>
2021-12-17 10:06:32 +0100

    reformatting clean.py and psd.py

--- src/clean.py
+++ src/clean.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-'''
+"""
 Created on Jul 13, 2015
 
 @author: Joerg Weingrill
@@ -9,10 +9,12 @@ Created on Jul 13, 2015
 @license: MIT
 @date: 2015-07-13
 @version: 1.0.0
-'''
+"""
 
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy import signal
+
 
 def dft(t, x):
     """
@@ -20,17 +22,18 @@ def dft(t, x):
     required by clean function
     """
     N = np.shape(t)[0]
-    df = 1/(2*(t[-1]-t[0]))
+    df = 1 / (2 * (t[-1] - t[0]))
     k = 8
-    j = np.arange(-k*N, k*N)
-    f = 2*j*df/k
-    A = np.ones([N, 2*k*N], dtype = complex)
-    
+    j = np.arange(-k * N, k * N)
+    f = 2 * j * df / k
+    A = np.ones([N, 2 * k * N], dtype=complex)
+
     A = A * (-2.j * np.pi * f)
     A = x * np.exp(A.transpose() * t)
     X = np.sum(A, axis=1) / N
-    assert(X.shape == f.shape)
+    assert (X.shape == f.shape)
     return X, f
+
 
 def idft(t, X, f=None):
     """
@@ -40,105 +43,110 @@ def idft(t, X, f=None):
     M = np.shape(f)[0]
     assert X.shape == f.shape
     k = 8
-    if f is None: 
+    if f is None:
         k = 8
-        df = 1/(2*(t[-1]-t[0]))
-        j = np.arange(-k*N, k*N)
-        f = j*df
-    A = np.ones([N, M], dtype = complex)
+        df = 1 / (2 * (t[-1] - t[0]))
+        j = np.arange(-k * N, k * N)
+        f = j * df
+    A = np.ones([N, M], dtype=complex)
     A = A * (2.j * np.pi * f)
     A = np.exp(A.transpose() * t).transpose() * X.transpose()
-    x = np.sum(A, axis=1) * N/2#/(2*k)
-    assert( t.shape == x.shape)
+    x = np.sum(A, axis=1) * N / 2  # /(2*k)
+    assert (t.shape == x.shape)
     return t, x
+
 
 def window(t, x):
     """
     calculates the window function of x(t)
     required by clean function
     """
-    assert( t.shape == x.shape)
+    assert (t.shape == x.shape)
     N = np.shape(t)[0]
-    df = 1/(1*(t[-1]-t[0]))
+    df = 1 / (1 * (t[-1] - t[0]))
     k = 16
-    j = np.arange(-k*N, k*N)
-    f = 2*j*df/k
-    A = np.ones([N, 2*k*N], dtype = complex)
-    
+    j = np.arange(-k * N, k * N)
+    f = 2 * j * df / k
+    A = np.ones([N, 2 * k * N], dtype=complex)
+
     A = A * (-2.j * np.pi * f)
     A = np.exp(A.transpose() * t)
     X = np.sum(A, axis=1) / N
-    assert( X.shape == f.shape)
+    assert (X.shape == f.shape)
     return X, f
 
-def clean_crane(t, x, g = 0.2):
+
+def clean_crane(t, x, g=0.2):
     """
     from Patrick C Crane: Solar Physics 203: 381-408, 2001
     """
     N = np.shape(t)[0]
     x -= np.mean(x)
-    dtmin = np.min(abs(t - np.roll(t,1)))
-    j = np.arange(-N, N+1)                                                # (A7)
-    assert(len(j) == 2*N + 1)
-    df = 1/(2*(t[-1]-t[0]))                                              # (A11)
-    f = j*df
-    print 'dmin = ',dtmin
-    print 'N =    ',N
-    print 'df =   ',df
-    f_c = 1./(2*dtmin)
+    dtmin = np.min(abs(t - np.roll(t, 1)))
+    j = np.arange(-N, N + 1)  # (A7)
+    assert (len(j) == 2 * N + 1)
+    df = 1 / (2 * (t[-1] - t[0]))  # (A11)
+    f = j * df
+    print('dmin = ', dtmin)
+    print('N =    ', N)
+    print('df =   ', df)
+    f_c = 1. / (2 * dtmin)
     fmax = N * df
-    print 'fmax = ',fmax
-    print 'fc   = ', f_c
-    #assert(fmax>=f_c)
-    
+    print('fmax = ', fmax)
+    print('fc   = ', f_c)
+
+    # assert(fmax>=f_c)
+
     def real(f):
-        re = np.zeros(2*N + 1)
-        for i in range(2*N + 1):
+        re = np.zeros(2 * N + 1)
+        for i in range(2 * N + 1):
             re[i] = np.sum(x * np.cos(2.0 * np.pi * f[i] * t)) / N
         return re
-    
+
     def imag(f):
-        im = np.zeros(2*N + 1)
-        for i in range(2*N + 1):
+        im = np.zeros(2 * N + 1)
+        for i in range(2 * N + 1):
             im[i] = np.sum(x * np.sin(2.0 * np.pi * f[i] * t)) / N
         return im
-    
-    def win(f): 
-        #k = np.arange(-2*N, 2*N+1)
-        #fk = k * df                                                      # (A13) 
-        w = np.zeros(2*N + 1, dtype = complex)
-        for i in range(2*N + 1):
-            w[i] = np.sum(np.exp( -2.j * np.pi * f[i] * t)) / N
+
+    def win(f):
+        # k = np.arange(-2*N, 2*N+1)
+        # fk = k * df                                                      # (A13)
+        w = np.zeros(2 * N + 1, dtype=complex)
+        for i in range(2 * N + 1):
+            w[i] = np.sum(np.exp(-2.j * np.pi * f[i] * t)) / N
         return w
-    #A = np.sqrt(R**2 + I**2)
-    X = real(f) - 1.j*imag(f)
-    
-    c = np.zeros(2*N + 1, dtype = complex)
+
+    # A = np.sqrt(R**2 + I**2)
+    X = real(f) - 1.j * imag(f)
+
+    c = np.zeros(2 * N + 1, dtype=complex)
     # clean iteration
-    
-    #1 search maximum in R
-    #R = real(f)
+
+    # 1 search maximum in R
+    # R = real(f)
     R = X
     for _ in range(N):
         p = np.argmax(abs(real[f >= 0.0])) + N
         Rp = R[p]
         fp = f[p]
-        assert(fp>=0.0)
-        
-        if fp>0:                                     # (A14a)
-            R = R - g*(Rp*win(f - fp) + np.conj(Rp)*win(f + fp))
-        elif fp == 0:                                # (A14b)
+        assert (fp >= 0.0)
+
+        if fp > 0:  # (A14a)
+            R = R - g * (Rp * win(f - fp) + np.conj(Rp) * win(f + fp))
+        elif fp == 0:  # (A14b)
             R -= g * Rp * win(f)
-        
-        if fp>0:                                     
-            c[f == fp] += g * Rp                     # (A15a)
-            c[f == -fp] += g * np.conj(Rp)           # (A15b)
+
+        if fp > 0:
+            c[f == fp] += g * Rp  # (A15a)
+            c[f == -fp] += g * np.conj(Rp)  # (A15b)
         elif fp == 0.0:
-            c[f == 0.0] += g * Rp                      # (A15c)
-    
+            c[f == 0.0] += g * Rp  # (A15c)
+
     return c, f
 
-def clean(t, x, gain = 0.1, threshold = 1e-4, maxiter = 1000):
+
+def clean(t, x, gain=0.1, threshold=1e-4, maxiter=1000):
     """implementation of the CLEAN algorithm after Roberts et al. (1987)
     :INPUTS:
        t -- (array) independent variable data
@@ -169,137 +177,142 @@ def clean(t, x, gain = 0.1, threshold = 1e-4, maxiter = 1000):
     :REQUIREMENTS:    :doc: `numpy`
     """
     import warnings
-    
+
     x -= np.mean(x)
     t -= t[0]
-    
+
     ft, f = dft(t, x)
     ftw, _ = window(t, x)
     aft = abs(ft)
     cleaned = np.zeros(len(ft))
     n2 = len(ft) / 2
     k = 0
-    
+
     for k in range(maxiter):
         i = np.argmax(aft[n2:])
         ipos = i + n2
         ineg = n2 - i
-        amp = gain*aft[ipos]
-        assert(aft[ipos]==aft[ineg])
-        assert(aft[ipos]==np.max(aft[n2:]))
+        amp = gain * aft[ipos]
+        assert (aft[ipos] == aft[ineg])
+        assert (aft[ipos] == np.max(aft[n2:]))
         sigma = np.std(aft[n2:])
-        if amp < gain*sigma or amp < threshold: break
-        
+        if amp < gain * sigma or amp < threshold: break
+
         cleaned[ipos] += amp
         cleaned[ineg] += amp
-        pftw = np.roll(abs(ftw), i)[n2:len(ft)+n2]
-        pftw += np.roll(abs(ftw), -i)[n2:len(ft)+n2]
-        aft = abs(aft - amp*pftw)
-    if k==maxiter-1:
+        pftw = np.roll(abs(ftw), i)[n2:len(ft) + n2]
+        pftw += np.roll(abs(ftw), -i)[n2:len(ft) + n2]
+        aft = abs(aft - amp * pftw)
+    if k == maxiter - 1:
         warnings.warn("maxiter reached!")
-    if k==0:
-        warnings.warn("no iterations were performed") 
-    from scipy import signal
+    if k == 0:
+        warnings.warn("no iterations were performed")
     gauss = signal.gaussian(len(cleaned), 2)
     cleaned = signal.convolve(cleaned, gauss, mode='same')
-    return f, cleaned+aft, aft
+    return f, cleaned + aft, aft
+
 
 def plot_lightcurve(t, x):
-    plt.plot(t, x*1000, 'ko-')
+    plt.plot(t, x * 1000, 'ko-')
     plt.minorticks_on()
-    plt.ylim(max(x*1000)+2, min(x*1000)-2)
-    plt.xlim(t[0],t[-1])
+    plt.ylim(max(x * 1000) + 2, min(x * 1000) - 2)
+    plt.xlim(t[0], t[-1])
     plt.ylabel('mmag')
-    
+
+
 def plot_dft(t, x, axis):
     ft, f = dft(t, x)
-    plt.plot(1./f[f>0.0], abs(ft)[f>0.0]/(2.0*np.var(x)), 'k')
+    plt.plot(1. / f[f > 0.0], abs(ft)[f > 0.0] / (2.0 * np.var(x)), 'k')
     plt.axvline(1, color='r', alpha=0.5)
-#    plt.axvline(p0, color='b')
+    #    plt.axvline(p0, color='b')
     i = np.argmax(ft)
-    period = 1./f[i]
+    period = 1. / f[i]
     plt.axvline(period, color='g', alpha=0.5)
 
-    
     plt.minorticks_on()
-    plt.xlim(0.0,max(t)/3)
+    plt.xlim(0.0, max(t) / 3)
     plt.ylabel('S(p)')
     plt.text(0.95, 0.9, 'DFT', va='top', horizontalalignment='right', transform=axis.transAxes)
 
+
 def plot_window(t, x, axis):
-    #window
+    # window
     ftw, fw = window(t, x)
-    plt.plot(1./fw[fw>0.0], abs(ftw)[fw>0.0], 'k')
-    plt.xlim(0.0,max(t)/2)
+    plt.plot(1. / fw[fw > 0.0], abs(ftw)[fw > 0.0], 'k')
+    plt.xlim(0.0, max(t) / 2)
     plt.text(0.95, 0.9, 'window', verticalalignment='top', horizontalalignment='right', transform=axis.transAxes)
 
+
 def plot_lomb(t, x, axis):
-    #lomb scargle
+    # lomb scargle
     import scipy.signal as signal
     nout = 1000
-    flomb = np.linspace(1./60., 1./0.05, nout)
+    flomb = np.linspace(1. / 60., 1. / 0.05, nout)
     pgram = signal.lombscargle(t, x, flomb)
-    norm = np.sqrt(4*(pgram/nout))/(2.0*np.var(x))
-    p = 2.*np.pi/flomb
-    plt.plot(p, norm,'k')
+    norm = np.sqrt(4 * (pgram / nout)) / (2.0 * np.var(x))
+    p = 2. * np.pi / flomb
+    plt.plot(p, norm, 'k')
 
     i = np.argmax(norm)
     period = p[i]
     plt.axvline(period, color='g', alpha=0.5)
     plt.axvline(1, color='r', alpha=0.5)
-#    plt.axvline(p0, color='b', alpha=0.5)
+    #    plt.axvline(p0, color='b', alpha=0.5)
 
-    
     plt.minorticks_on()
-    plt.xlim(0.0,max(t)/3)
+    plt.xlim(0.0, max(t) / 3)
     plt.ylabel('S(p)')
     plt.text(0.95, 0.9, 'Lomb-Scargle', verticalalignment='top', horizontalalignment='right', transform=axis.transAxes)
 
+
 def plot_clean(t, x, axis):
     f, cleaned, _ = clean(t, x, gain=0.9, threshold=2e-3)
-    n2 = len(f) /2
-    cf = cleaned[n2+1:]/(2.0*np.var(x))
-    p = 1./f[n2+1:]
+    n2 = len(f) / 2
+    cf = cleaned[n2 + 1:] / (2.0 * np.var(x))
+    p = 1. / f[n2 + 1:]
     cf = cf
     p = p
     i = np.argmax(cf)
     period = p[i]
-    
+
     plt.axvline(1, color='r', alpha=0.5)
-#    plt.axvline(p0, color='b', alpha=0.5)
+    #    plt.axvline(p0, color='b', alpha=0.5)
     plt.axvline(period, color='g', alpha=0.5)
-    plt.xlim(0.0,max(t)/3)
+    plt.xlim(0.0, max(t) / 3)
     plt.plot(p, cf, 'k')
     plt.minorticks_on()
     plt.ylabel('S(p)')
     plt.text(0.95, 0.9, 'CLEAN', verticalalignment='top', horizontalalignment='right', transform=axis.transAxes)
     return period
 
+
 def plot_phase(t, x, period):
     from functions import phase
     tp, yp = phase(t, x, period)
-    plt.scatter(tp,yp*1000, c='k')
-    plt.scatter(tp+period,yp*1000, c='k')
-    plt.xlim(0.0,period*2)
-    plt.ylim(min(yp)*1000,max(yp)*1000)
+    plt.scatter(tp, yp * 1000, c='k')
+    plt.scatter(tp + period, yp * 1000, c='k')
+    plt.xlim(0.0, period * 2)
+    plt.ylim(min(yp) * 1000, max(yp) * 1000)
     plt.xlabel('period = %.2f' % period)
     plt.axvline(period, linestyle='--', color='k')
     plt.minorticks_on()
     plt.ylabel('mmag')
     plt.ylim(plt.ylim()[::-1])
-    
+
+
 def do_shuffle(t, x):
     from random import shuffle
     shuffle(x)
     return t, x
-    
+
 
 def detrend(t, x):
     par = np.polyfit(t, x, 1)
-    x -= par[0]*t + par[1]
+    x -= par[0] * t + par[1]
     return t, x
 
-def loadfromfile(filename= None):
+
+def loadfromfile(filename=None):
     if filename is None:
         from glob import glob
         filenames = glob('/work2/jwe/SOCS/M48/lightcurves.new/*.dat')
@@ -309,94 +322,96 @@ def loadfromfile(filename= None):
     x -= np.mean(x)
     return t, x
 
+
 def simulate():
-    t = np.linspace(0,63, 100)
+    t = np.linspace(0, 63, 100)
     p0 = 3.24
-    x = 0.3*np.sin(2.*np.pi*t/p0)+0.2
+    x = 0.3 * np.sin(2. * np.pi * t / p0) + 0.2
     x += np.random.normal(scale=0.3, size=x.shape)
     return t, x
+
 
 def acorr(t, x):
     p, f = dft(t, x)
     pc = p.conjugate()
     t1 = np.linspace(t[0], t[-1], 1000)
-    ti , xi = idft(t1, p*pc, f=f)
+    ti, xi = idft(t1, p * pc, f=f)
     return ti, xi
-    
+
 
 def plot_acorr(t, x, axis):
     ta, ac = acorr(t, x)
-    print ta.shape, ac.shape
+    print(ta.shape, ac.shape)
     plt.plot(ta, ac, 'k')
     plt.axvline(1, color='r', alpha=0.5)
-#    plt.axvline(period, color='g', alpha=0.5)
-    plt.xlim(0.0,max(t)/3)
+    #    plt.axvline(period, color='g', alpha=0.5)
+    plt.xlim(0.0, max(t) / 3)
     plt.minorticks_on()
     plt.ylabel('S(p)')
     plt.text(0.95, 0.9, 'acorr', verticalalignment='top', horizontalalignment='right', transform=axis.transAxes)
 
+
 def plot_finterpol(t, x):
-    p,f = dft(t, x)
+    p, f = dft(t, x)
     from scipy import signal
     n = 1000
     gaussian_window = signal.gaussian(p.shape[0], std=150)
     p = p * gaussian_window
     t1 = np.linspace(t[0], t[-1], n)
-    ti , xi = idft(t1, p, f=f)
-    
-    print ti.shape, xi.shape
-    plt.scatter(t, x* 1000, alpha = 0.5)
+    ti, xi = idft(t1, p, f=f)
+
+    print(ti.shape, xi.shape)
+    plt.scatter(t, x * 1000, alpha=0.5)
     plt.plot(ti, xi, 'k')
-    plt.xlim(ti[0],ti[-1])
+    plt.xlim(ti[0], ti[-1])
     plt.minorticks_on()
     plt.ylabel('mag')
-    #plt.text(0.95, 0.9, 'acorr', verticalalignment='top', horizontalalignment='right', transform=axis.transAxes)
-    
+    # plt.text(0.95, 0.9, 'acorr', verticalalignment='top', horizontalalignment='right', transform=axis.transAxes)
+
+
 def _test():
     filename = '/work2/jwe/SOCS/M48/lightcurves/20140303A-0074-0013#1952.dat'
-    t, x = loadfromfile(filename)    
+    t, x = loadfromfile(filename)
     t, x = detrend(t, x)
 
     from matplotlib import rcParams
     params = {'backend': 'Agg',
-      'axes.labelsize': 8,
-      'axes.titlesize': 10,
-      'font.size': 8,
-      'xtick.labelsize': 8,
-      'ytick.labelsize': 8,
-      'figure.figsize': [17/2.54, 24/2.54],
-      'savefig.dpi' : 300,
-      'font.family': 'sans-serif',
-      'axes.linewidth' : 0.5,
-      #'xtick.major.size' : 2,
-      #'ytick.major.size' : 2,
-      }
+              'axes.labelsize': 8,
+              'axes.titlesize': 10,
+              'font.size': 8,
+              'xtick.labelsize': 8,
+              'ytick.labelsize': 8,
+              'figure.figsize': [17 / 2.54, 24 / 2.54],
+              'savefig.dpi': 300,
+              'font.family': 'sans-serif',
+              'axes.linewidth': 0.5,
+              # 'xtick.major.size' : 2,
+              # 'ytick.major.size' : 2,
+              }
     rcParams.update(params)
-    
+
     plt.figure()
     plt.subplot('511')
     plt.title(filename[32:-4])
     plot_lightcurve(t, x)
-    
+
     axis2 = plt.subplot('512')
     plot_dft(t, x, axis2)
-    #sigma = np.std(ft[f>=0])
-    
+    # sigma = np.std(ft[f>=0])
+
     plt.subplot('513')
-    #plot_lomb(t, x)
-    #plot_acorr(t, x)
+    # plot_lomb(t, x)
+    # plot_acorr(t, x)
     plot_finterpol(t, x)
-        
+
     axis4 = plt.subplot('514')
-    period = plot_clean(t, x,axis4)
+    period = plot_clean(t, x, axis4)
 
     plt.subplot('515')
     plot_phase(t, x, period)
-    
-        
+
     plt.savefig('/home/jwe/Pictures/Figures/clean.pdf')
     plt.close()
-    
 
 
 if __name__ == '__main__':
--- src/psd.py
+++ src/psd.py
@@ -4,12 +4,14 @@ Created on Apr 8, 2013
 @author: jwe <jweingrill@aip.de>
 '''
 
+
 def _init(argst, argsy):
     global t_global
     global y_global
-        
+
     t_global = argst
     y_global = argsy
+
 
 def _worker(wi):
     from numpy import array, cos, sin, dot
@@ -17,13 +19,14 @@ def _worker(wi):
 
     global t_global
     global y_global
-        
+
     N = len(t_global)
-    A = array([[cos(wi*ti),sin(wi*ti)] for ti in t_global])
+    A = array([[cos(wi * ti), sin(wi * ti)] for ti in t_global])
     AT = A.T
-    R = dot(AT, A) 
+    R = dot(AT, A)
     r = dot(AT, y_global)
-    return dot(dot(r.T,inv(R)),r)/N
+    return dot(dot(r.T, inv(R)), r) / N
+
 
 def ppsd(t, y, lower=0.001, upper=0.5, num=None, logspace=False):
     '''
@@ -41,22 +44,23 @@ def ppsd(t, y, lower=0.001, upper=0.5, num=None, logspace=False):
     if num is None:
         num = len(y)
     if logspace:
-        w = 2.0*pi*logspace(lower, upper, num)
+        w = 2.0 * pi * logspace(lower, upper, num)
     else:
-        w = 2.0*pi*linspace(lower, upper, num)
+        w = 2.0 * pi * linspace(lower, upper, num)
 
-    pool = Pool(initializer=_init, initargs=(t,y))
+    pool = Pool(initializer=_init, initargs=(t, y))
     p = pool.map(_worker, w)
-    pool.close() # no more tasks
+    pool.close()  # no more tasks
     pool.join()  # wrap up current tasks
-    return array(p), w/(2.0*pi)
+    return array(p), w / (2.0 * pi)
+
 
 def mpsd(t, y):
     from multiprocessing import Queue, Process
 
     from numpy import linspace, array, empty, cos, sin, dot, argsort
     from numpy.linalg import inv
-    
+
     def worker(tasks, results):
         # get a task
         task = tasks.get()
@@ -65,19 +69,18 @@ def mpsd(t, y):
         y = task['y']
         N = len(y)
         # do operation
-        A = array([[cos(wi*ti),sin(wi*ti)] for ti in t])
+        A = array([[cos(wi * ti), sin(wi * ti)] for ti in t])
         AT = A.T
-        R = dot(AT, A) 
+        R = dot(AT, A)
         r = dot(AT, y)
-        result = dot(dot(r.T,inv(R)),r)/N
+        result = dot(dot(r.T, inv(R)), r) / N
 
         # put the result in results queue
-        results.put([wi/(2.0*pi),result])
-    
+        results.put([wi / (2.0 * pi), result])
 
     N = len(y)
-    
-    w = 2.0*pi*linspace(0.001, 0.5, N)
+
+    w = 2.0 * pi * linspace(0.001, 0.5, N)
     f_result = empty(len(w))
     P_result = empty(len(w))
 
@@ -85,19 +88,20 @@ def mpsd(t, y):
     myTasks = Queue()
     myResults = Queue()
     Workers = [Process(target=worker, args=(myTasks, myResults)) for i in range(N)]
-    
+
     for each in Workers:
         each.start()
-        
+
     for i in range(N):
-        task = {'wi':w[i], 't':t, 'y':y}
+        task = {'wi': w[i], 't': t, 'y': y}
         myTasks.put(task)
-        
+
     while N:
-        f_result[N-1],P_result[N-1] = myResults.get()
+        f_result[N - 1], P_result[N - 1] = myResults.get()
         N -= 1
-    k = argsort(f_result)    
-    return [P_result[i] for i in k], [f_result[i] for i in k]   
+    k = argsort(f_result)
+    return [P_result[i] for i in k], [f_result[i] for i in k]
+
 
 def psd(t, y, lower=0.001, upper=0.5):
     """
@@ -106,57 +110,60 @@ def psd(t, y, lower=0.001, upper=0.5):
     """
     from numpy import linspace, array, empty, cos, sin, dot, pi
     from numpy.linalg import inv
-    
-    N = len(y)
-    
-    w = 2.0*pi*linspace(lower, upper, N)
+
+    n_y = len(y)
+
+    w = 2.0 * pi * linspace(lower, upper, n_y)
     result = empty(len(w))
     i = 0
     for wi in w:
-        A = array([[cos(wi*ti),sin(wi*ti)] for ti in t])
+        A = array([[cos(wi * ti), sin(wi * ti)] for ti in t])
         AT = A.T
-        R = dot(AT, A) 
+        R = dot(AT, A)
         r = dot(AT, y)
-        result[i] = dot(dot(r.T,inv(R)),r)/N
+        result[i] = dot(dot(r.T, inv(R)), r) / n_y
         i += 1
-    return result,w/(2.0*pi)
+    return result, w / (2.0 * pi)
+
 
 if __name__ == '__main__':
     from numpy import cos, pi, sqrt
     import numpy as np
-    #n = arange(30)
-    
+
+    # n = arange(30)
+
     N = 20
-    n = np.random.random_sample((N,))*N
+    n = np.random.random_sample((N,)) * N
     m = np.linspace(0.0, 20.0, 200)
     n.sort()
     f1 = 0.25
     f2 = 0.4
     phi1 = 0.0
     phi2 = 0.784357
-    y = 0.0*cos(2*pi*f1*n+phi1) + 8.0*cos(2*pi*f2*n+phi2)
-    z = 0.0*cos(2*pi*f1*m+phi1) + 8.0*cos(2*pi*f2*m+phi2)
-    px, f = ppsd(n,y)
-    #px, f = psd(n,y)
+    y = 0.0 * cos(2 * pi * f1 * n + phi1) + 8.0 * cos(2 * pi * f2 * n + phi2)
+    z = 0.0 * cos(2 * pi * f1 * m + phi1) + 8.0 * cos(2 * pi * f2 * m + phi2)
+    px, f = ppsd(n, y)
+    # px, f = psd(n,y)
     px = sqrt(px)
-    
+
     import matplotlib
+
     matplotlib.use('WXAgg')
     import matplotlib.pyplot as plt
-    
+
     fig = plt.figure()
-    ax = fig.add_subplot(2,1,1)
+    ax = fig.add_subplot(2, 1, 1)
     ax.set_xlabel('n')
     ax.set_ylabel('$y_n$')
     ax.plot(n, y, '.')
     ax.plot(n, y, '--')
     ax.plot(m, z, 'g')
-    
-    ax = fig.add_subplot(2,1,2)
+
+    ax = fig.add_subplot(2, 1, 2)
     ax.set_xlabel('f')
     ax.set_ylabel('P($\omega$)')
     ax.grid()
     ax.plot(f, px)
-    
+
     plt.show()
     pass
